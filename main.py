85ab9d0a178baaa76cf7468f3e394d75c018c189
Yam Alao <afk.puma@gmail.com>
2023-09-08 04:28:07 -0300

    trash_path

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-msg = "ola"
-nome = "yas"
+import send2trash
+import os
+
+# Substitua o caminho abaixo pelo caminho da sua lixeira
+trash_path = 'C:\\$Recycle.Bin'
+
+for root, dirs, files in os.walk(trash_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        send2trash.send2trash(file_path)
